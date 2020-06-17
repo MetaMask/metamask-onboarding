c3be547958ea35ac102207ac8a2029faf3d33ad6
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2020-06-17 15:22:05 -0230

    Migrate to TypeScript (#14)

--- rollup.config.standalone.js
+++ rollup.config.standalone.js
@@ -1,14 +1,22 @@
-import resolve from 'rollup-plugin-node-resolve'
+import typescript from '@rollup/plugin-typescript';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 
 export default {
-  input: 'src/index.js',
-  output: {
+  input: 'src/index.ts',
+  output: [{
     file: 'dist/metamask-onboarding.bundle.js',
     format: 'iife',
     name: 'MetamaskOnboarding',
-  },
+  }],
   plugins: [
+    typescript(),
     resolve(),
+    commonjs({
+      namedExports: {
+        'bowser': ['parse'],
+      },
+    }),
   ],
-}
+};
 
