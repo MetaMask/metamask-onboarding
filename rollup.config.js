1def87f8cfdce52145e9cacf5c8301c3bd645546
Brad Decker <git@braddecker.dev>
2020-06-23 15:53:48 -0500

    bundle for ES6 and ship types

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,40 @@
-import typescript from '@rollup/plugin-typescript';
+import typescript from 'rollup-plugin-typescript2';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 
-export default {
-  external: ['bowser'],
-  input: 'src/index.ts',
-  output: [
-    {
-      file: 'dist/metamask-onboarding.cjs.js',
-      format: 'cjs',
-    },
-  ],
-  plugins: [
-    typescript(),
-  ],
-};
+const config = [
+  {
+    external: ['bowser'],
+    input: 'src/index.ts',
+    output: [
+      {
+        file: 'dist/metamask-onboarding.cjs.js',
+        format: 'cjs',
+      },
+      {
+        file: 'dist/metamask-onboarding.es.js',
+        format: 'es',
+      },
+    ],
+    plugins: [
+      typescript(),
+    ],
+  }, {
+    input: 'src/index.ts',
+    output: [{
+      file: 'dist/metamask-onboarding.bundle.js',
+      format: 'iife',
+      name: 'MetamaskOnboarding',
+    }],
+    plugins: [
+      typescript(),
+      resolve(),
+      commonjs({
+        namedExports: {
+          'bowser': ['parse'],
+        },
+      }),
+    ],
+  }];
+
+export default config;
